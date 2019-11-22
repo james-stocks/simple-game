754f12e28078516d180cffe632fdc4b95c4ef463
James Stocks <jstocks@gmail.com>
2019-11-22 23:52:03 +0000

    Transition from title screen to playing

--- main.go
+++ main.go
@@ -29,12 +29,36 @@ func loadPicture(path string) (pixel.Picture, error) {
 }
 
 func run() {
+    type GameState int
+	const (
+	    GameStateTitleScreen GameState = iota
+		GameStatePlaying
+		GameStateGameOver
+	)
+	
+	gameState := GameStateTitleScreen
+	
+	type GameScreen int
+	const (
+	    GameScreenVillage GameScreen = iota
+		GameScreenForest
+	)
+	
+	gameScreen := GameScreenVillage
+	
+	// Load background images
+	
     backgroundTitlePic, err := loadPicture("assets/backgrounds/title.png")
 	if err != nil {
 		panic(err)
 	}
-
     backgroundTitleSprite := pixel.NewSprite(backgroundTitlePic, backgroundTitlePic.Bounds())
+
+    backgroundVillagePic, err := loadPicture("assets/backgrounds/village.png")
+	if err != nil {
+		panic(err)
+	}
+	backgroundVillageSprite := pixel.NewSprite(backgroundVillagePic, backgroundVillagePic.Bounds())
 
     cfg := pixelgl.WindowConfig{
 		Title:  "Adventure Game",
@@ -49,15 +73,34 @@ func run() {
     basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
 	titleInfoTxt := text.New(pixel.V(800,50), basicAtlas)
 	fmt.Fprintln(titleInfoTxt, "Created by James Stocks")
+	titlePromptTxt := text.New(pixel.V(400,300), basicAtlas)
+	fmt.Fprintln(titlePromptTxt, "Press Enter to start your adventure")
 
     fps60 := time.Tick(time.Second / 60)
 
 	for !win.Closed() {
+	    // Update the game
+		switch gameState {
+		case GameStateTitleScreen:
+			if win.JustPressed(pixelgl.KeyEnter) {
+		        gameState = GameStatePlaying
+		    }
+		}
+		
+	    // Draw the screen
 	    win.Clear(pixel.RGB(0, 0, 0))
 		
-		backgroundTitleSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
-		
-		titleInfoTxt.Draw(win, pixel.IM)
+		switch gameState {
+		case GameStateTitleScreen:
+		    backgroundTitleSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+		    titleInfoTxt.Draw(win, pixel.IM)
+			titlePromptTxt.Draw(win, pixel.IM)
+        case GameStatePlaying:
+		    switch gameScreen {
+			case GameScreenVillage:
+			    backgroundVillageSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+			}
+		}
 		
 		win.Update()
 
