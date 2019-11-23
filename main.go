d3e347f00139162a17debb9f306eab483c743b50
James Stocks <jstocks@gmail.com>
2019-11-23 00:52:30 +0000

    Transition from village to quest

--- main.go
+++ main.go
@@ -62,6 +62,12 @@ func run() {
 	}
 	backgroundVillageSprite := pixel.NewSprite(backgroundVillagePic, backgroundVillagePic.Bounds())
 
+    backgroundForestPic, err := loadPicture("assets/backgrounds/forest.png")
+	if err != nil {
+		panic(err)
+	}
+	backgroundForestSprite := pixel.NewSprite(backgroundForestPic, backgroundForestPic.Bounds())
+
     cfg := pixelgl.WindowConfig{
 		Title:  "Adventure Game",
 		Bounds: pixel.R(0, 0, 1024, 768),
@@ -78,11 +84,18 @@ func run() {
 	titlePromptTxt := text.New(pixel.V(400,300), basicAtlas)
 	fmt.Fprintln(titlePromptTxt, "Press Enter to start your adventure")
 
+    villagePromptTxt := text.New(pixel.V(800, 50), basicAtlas)
+	fmt.Fprintln(villagePromptTxt, "-> Begin Quest")
+
+    questStepsRemainingTxt := text.New(pixel.V(800, 700), basicAtlas)
+
     playerLevelTxt := text.New(pixel.V(20, 700), basicAtlas)
 
     fps60 := time.Tick(time.Second / 60)
 
     player := player.Player{ 1, 0 }
+    isInQuest := false
+	questStepsRemaining := 0
 
 	for !win.Closed() {
 	    // Update the game
@@ -94,6 +107,15 @@ func run() {
 		case GameStatePlaying:
 		    playerLevelTxt.Clear()
 			fmt.Fprintln(playerLevelTxt, "Player level:", player.Level)
+			switch gameScreen {
+			case GameScreenVillage:
+			    if win.JustPressed(pixelgl.KeyRight) {
+				    gameScreen = GameScreenForest
+					isInQuest = true
+					questStepsRemaining = 5
+					fmt.Fprintln(questStepsRemainingTxt, "Quest steps remaining:", questStepsRemaining)
+				}
+			}
 		}
 		
 	    // Draw the screen
@@ -108,9 +130,15 @@ func run() {
 		    switch gameScreen {
 			case GameScreenVillage:
 			    backgroundVillageSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+				villagePromptTxt.Draw(win, pixel.IM)
+			case GameScreenForest:
+			    backgroundForestSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 			}
 			// Draw UI common to all screens
 			playerLevelTxt.Draw(win, pixel.IM)
+			if isInQuest == true {
+			   questStepsRemainingTxt.Draw(win, pixel.IM)
+			}
 		}
 		
 		win.Update()
